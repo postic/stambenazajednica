cc08a975d94d09cec93e980b31f447f7be8a4b3f
stevap <postic.stevan@gmail.com>
2026-03-08 21:11:32 +0100

    vercel deploy problem

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+
+  experimental: {},
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+};
+
+export default nextConfig;
+
+/*import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
   experimental: {
     allowedDevOrigins: [
       "https://nextjs.ddev.site:3001",
@@ -15,5 +45,5 @@ const nextConfig: NextConfig = {
     domains: ["drupal.ddev.site"],
   },
 };
-
 export default nextConfig;
+*/
