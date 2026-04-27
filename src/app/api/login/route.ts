3a69475be2406498268b9fcece1ac4d7876046fb
stevap <postic.stevan@gmail.com>
2026-04-27 21:53:09 +0200

    Build

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: NextRequest) {
     // =========================
     if (role === "stanar") {
       res = await fetch(
-        `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/api/stan-login`,
+        `https://dev-stambena-zajednica.pantheonsite.io/api/stan-login`,
         {
           method: "POST",
           headers: {
@@ -39,7 +39,7 @@ export async function POST(req: NextRequest) {
       });
 
       res = await fetch(
-        `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/oauth/token`,
+        `https://dev-stambena-zajednica.pantheonsite.io/oauth/token`,
         {
           method: "POST",
           headers: {
