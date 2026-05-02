83f8fda421d9a1d8da2564b3368efe136affa3e5
stevap <postic.stevan@gmail.com>
2026-05-02 15:11:23 +0200

    Build

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                   <BalancePill />
                 </div>
 
-                <AllowNotifications />
+                {/* <AllowNotifications /> */}
 
               </main>
             </div>
