bda9a9807dc9646cae47eb7b78ba911117fe2a67
stevap <postic.stevan@gmail.com>
2026-05-07 19:27:46 +0200

    Kvarovi

--- src/features/stanovi/StanoviColumns.tsx
+++ src/features/stanovi/StanoviColumns.tsx
@@ -2,83 +2,59 @@ import { Column } from "@/components/table/types";
 import type { Stan } from "@/types/stan";
 import { FaEye, FaEdit, FaTrash } from "react-icons/fa";
 import Link from "next/link";
+import { toRoman } from "@/lib/text";
 
 export const stanoviColumns: Column<Stan>[] = [
   {
     key: "title",
     header: "Naziv",
-    //width: "50%",
-    //sortable: false,
     render: (stan) => (
       <Link
         href={`/stanovi/${stan.id}`}
-        className=" hover:underline"
+        className="hover:underline"
         title={stan.title}
       >
         {stan.title}
       </Link>
     ),
   },
+
   {
     key: "vlasnik",
     header: "Vlasnik",
-    render: (stan) => (
-      <span>{stan.vlasnik ?? "-"}</span>
-    )
+    render: (stan) => <span>{stan.vlasnik ?? "-"}</span>,
   },
+
   {
     key: "stanari",
     header: "Stanari",
-    render: (stan) => (
-      <span>{stan.broj_stanara ?? "-"}</span>
-    )
+    render: (stan) => <span>{stan.broj_stanara ?? "-"}</span>,
   },
+
   {
     key: "sprat",
     header: "Sprat",
-    render: (stan) => (
-      <span>{stan.sprat ?? "-"}</span>
-    )
+    render: (stan) => {
+      const sprat =
+        typeof stan.sprat === "number"
+          ? toRoman(stan.sprat)
+          : stan.sprat != null
+            ? toRoman(Number(stan.sprat))
+            : null;
+
+      return <span>{sprat ?? "-"}</span>;
+    },
   },
+
   {
-    key: "porsina",
+    key: "povrsina",
     header: "Površina",
     render: (stan) => (
       <span>
         {stan.kvadratura != null
-        ? `${Number(stan.kvadratura).toLocaleString("sr-Latn-RS")} m²`
-        : "-"}
+          ? `${Number(stan.kvadratura).toLocaleString("sr-Latn-RS")} m²`
+          : "-"}
       </span>
-    )
-  },
-  {
-    key: "actions",
-    header: "Akcije",
-    width: "90px", // 👈 KLJUČNO
-    isAction: true,
-    render: (stan) => (
-      <div className="flex justify-center gap-2">
-        <Link
-          href={`/stanovi/${stan.id}`}
-          className=" hover:text-blue-800"
-          title="View"
-        >
-          <FaEye />
-        </Link>
-        <Link
-          href={`/stanovi/${stan.id}/edit`}
-          className="text-yellow-600 hover:text-yellow-800"
-          title="Edit"
-        >
-          <FaEdit />
-        </Link>
-        <button
-          className="text-red-600 hover:text-red-800"
-          title="Delete"
-        >
-          <FaTrash />
-        </button>
-      </div>
     ),
   },
 ];
